b3ebb3328f9dbad8ed698040c5316601412b8b66
Saer El Masri <saer1890@gmail.com>
2023-12-13 17:05:16 +0200

    SDK update generated by liblab

--- test/services/packages/Packages.test.ts
+++ test/services/packages/Packages.test.ts
@@ -23,19 +23,19 @@ describe('test Packages', () => {
     test('test api call', () => {
       const scope = nock('https://api.celitech.net/v1')
         .get(
-          '/packages?destination=ab&startDate=voluptatem&endDate=quos&afterCursor=aut&limit=8&startTime=2&endTime=2&duration=4',
+          '/packages?destination=saepe&startDate=impedit&endDate=harum&afterCursor=asperiores&limit=6&startTime=4&endTime=3&duration=9',
         )
         .reply(200, { data: {} });
       return sdk.packages
         .listPackages({
-          destination: 'ab',
-          startDate: 'voluptatem',
-          endDate: 'quos',
-          afterCursor: 'aut',
-          limit: 8,
-          startTime: 2,
-          endTime: 2,
-          duration: 4,
+          destination: 'saepe',
+          startDate: 'impedit',
+          endDate: 'harum',
+          afterCursor: 'asperiores',
+          limit: 6,
+          startTime: 4,
+          endTime: 3,
+          duration: 9,
         })
         .then((r: any) => expect(r.data).toEqual({}));
     });
--- test/services/purchases/Purchases.test.ts
+++ test/services/purchases/Purchases.test.ts
@@ -23,18 +23,18 @@ describe('test Purchases', () => {
     test('test api call', () => {
       const scope = nock('https://api.celitech.net/v1')
         .get(
-          '/purchases?iccid=5695200829&afterDate=mollitia&beforeDate=nisi&afterCursor=modi&limit=1&after=4&before=8',
+          '/purchases?iccid=3486219247&afterDate=eius&beforeDate=architecto&afterCursor=vitae&limit=7&after=9&before=3',
         )
         .reply(200, { data: {} });
       return sdk.purchases
         .listPurchases({
-          iccid: '5695200829',
-          afterDate: 'mollitia',
-          beforeDate: 'nisi',
-          afterCursor: 'modi',
-          limit: 1,
-          after: 4,
-          before: 8,
+          iccid: '3486219247',
+          afterDate: 'eius',
+          beforeDate: 'architecto',
+          afterCursor: 'vitae',
+          limit: 7,
+          after: 9,
+          before: 3,
         })
         .then((r: any) => expect(r.data).toEqual({}));
     });
@@ -68,26 +68,26 @@ describe('test Purchases', () => {
   describe('test getPurchaseConsumption', () => {
     test('test api call', () => {
       const scope = nock('https://api.celitech.net/v1')
-        .get('/purchases/itaque/consumption')
+        .get('/purchases/totam/consumption')
         .reply(200, { data: {} });
       return sdk.purchases
-        .getPurchaseConsumption('itaque')
+        .getPurchaseConsumption('totam')
         .then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://api.celitech.net/v1')
-        .get('/purchases/labore/consumption')
+        .get('/purchases/aliquam/consumption')
         .reply(200, { data: {} });
       return expect(async () => await sdk.purchases.getPurchaseConsumption()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://api.celitech.net/v1')
-        .get('/purchases/sed/consumption')
+        .get('/purchases/natus/consumption')
         .reply(404, { data: {} });
       return expect(
-        async () => await sdk.purchases.getPurchaseConsumption('sed'),
+        async () => await sdk.purchases.getPurchaseConsumption('natus'),
       ).rejects.toThrow();
     });
   });

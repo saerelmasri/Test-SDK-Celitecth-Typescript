8480b9af2fb92c04d567cb53c839722759cda9ec
Saer El Masri <saer1890@gmail.com>
2024-01-09 17:13:58 +0200

    SDK update generated by liblab

--- test/services/eSim/ESim.test.ts
+++ test/services/eSim/ESim.test.ts
@@ -1,6 +1,6 @@
 import nock from 'nock';
 
-import { Testcelitechliblab } from '../../../src';
+import { Celitech } from '../../../src';
 
 import { ESimService } from '../../../src/services/eSim/ESim';
 
@@ -14,7 +14,7 @@ describe('test ESim', () => {
   let sdk: any;
 
   beforeEach(() => {
-    sdk = new Testcelitechliblab();
+    sdk = new Celitech();
 
     nock.cleanAll();
   });
@@ -22,92 +22,92 @@ describe('test ESim', () => {
   describe('test getEsim', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim?iccid=2647886229')
+        .get('/esim?iccid=1383996592')
         .reply(200, { data: {} });
-      return sdk.eSim.getEsim('2647886229').then((r: any) => expect(r.data).toEqual({}));
+      return sdk.eSim.getEsim('1383996592').then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim?iccid=7214750268')
+        .get('/esim?iccid=2503065315')
         .reply(200, { data: {} });
       return expect(async () => await sdk.eSim.getEsim()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim?iccid=6886922355')
+        .get('/esim?iccid=4872040946')
         .reply(404, { data: {} });
-      return expect(async () => await sdk.eSim.getEsim('6886922355')).rejects.toThrow();
+      return expect(async () => await sdk.eSim.getEsim('4872040946')).rejects.toThrow();
     });
   });
 
   describe('test getEsimDevice', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/6014370256/device')
+        .get('/esim/9313177037/device')
         .reply(200, { data: {} });
-      return sdk.eSim.getEsimDevice('6014370256').then((r: any) => expect(r.data).toEqual({}));
+      return sdk.eSim.getEsimDevice('9313177037').then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/1422546034/device')
+        .get('/esim/7538410929/device')
         .reply(200, { data: {} });
       return expect(async () => await sdk.eSim.getEsimDevice()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/2397901450/device')
+        .get('/esim/6161415951/device')
         .reply(404, { data: {} });
-      return expect(async () => await sdk.eSim.getEsimDevice('2397901450')).rejects.toThrow();
+      return expect(async () => await sdk.eSim.getEsimDevice('6161415951')).rejects.toThrow();
     });
   });
 
   describe('test getEsimHistory', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/8569190098/history')
+        .get('/esim/6142410842/history')
         .reply(200, { data: {} });
-      return sdk.eSim.getEsimHistory('8569190098').then((r: any) => expect(r.data).toEqual({}));
+      return sdk.eSim.getEsimHistory('6142410842').then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/8088799051/history')
+        .get('/esim/3786980604/history')
         .reply(200, { data: {} });
       return expect(async () => await sdk.eSim.getEsimHistory()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/8551219600/history')
+        .get('/esim/2157056859/history')
         .reply(404, { data: {} });
-      return expect(async () => await sdk.eSim.getEsimHistory('8551219600')).rejects.toThrow();
+      return expect(async () => await sdk.eSim.getEsimHistory('2157056859')).rejects.toThrow();
     });
   });
 
   describe('test getEsimMac', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/3607980330/mac')
+        .get('/esim/9297418158/mac')
         .reply(200, { data: {} });
-      return sdk.eSim.getEsimMac('3607980330').then((r: any) => expect(r.data).toEqual({}));
+      return sdk.eSim.getEsimMac('9297418158').then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/2719268674/mac')
+        .get('/esim/4664481607/mac')
         .reply(200, { data: {} });
       return expect(async () => await sdk.eSim.getEsimMac()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/esim/3751541804/mac')
+        .get('/esim/1665972863/mac')
         .reply(404, { data: {} });
-      return expect(async () => await sdk.eSim.getEsimMac('3751541804')).rejects.toThrow();
+      return expect(async () => await sdk.eSim.getEsimMac('1665972863')).rejects.toThrow();
     });
   });
 });
--- test/services/purchases/Purchases.test.ts
+++ test/services/purchases/Purchases.test.ts
@@ -1,6 +1,6 @@
 import nock from 'nock';
 
-import { Testcelitechliblab } from '../../../src';
+import { Celitech } from '../../../src';
 
 import { PurchasesService } from '../../../src/services/purchases/Purchases';
 
@@ -14,7 +14,7 @@ describe('test Purchases', () => {
   let sdk: any;
 
   beforeEach(() => {
-    sdk = new Testcelitechliblab();
+    sdk = new Celitech();
 
     nock.cleanAll();
   });
@@ -23,18 +23,18 @@ describe('test Purchases', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
         .get(
-          '/purchases?iccid=6273297066&afterDate=quas&beforeDate=explicabo&afterCursor=veritatis&limit=3&after=4&before=8',
+          '/purchases?iccid=5979871490&afterDate=itaque&beforeDate=vitae&afterCursor=magni&limit=9&after=3&before=9',
         )
         .reply(200, { data: {} });
       return sdk.purchases
         .listPurchases({
-          iccid: '6273297066',
-          afterDate: 'quas',
-          beforeDate: 'explicabo',
-          afterCursor: 'veritatis',
-          limit: 3,
-          after: 4,
-          before: 8,
+          iccid: '5979871490',
+          afterDate: 'itaque',
+          beforeDate: 'vitae',
+          afterCursor: 'magni',
+          limit: 9,
+          after: 3,
+          before: 9,
         })
         .then((r: any) => expect(r.data).toEqual({}));
     });
@@ -70,27 +70,25 @@ describe('test Purchases', () => {
   describe('test getPurchaseConsumption', () => {
     test('test api call', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/purchases/voluptates/consumption')
+        .get('/purchases/eius/consumption')
         .reply(200, { data: {} });
       return sdk.purchases
-        .getPurchaseConsumption('voluptates')
+        .getPurchaseConsumption('eius')
         .then((r: any) => expect(r.data).toEqual({}));
     });
 
     test('test will throw error if required fields missing', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/purchases/modi/consumption')
+        .get('/purchases/molestias/consumption')
         .reply(200, { data: {} });
       return expect(async () => await sdk.purchases.getPurchaseConsumption()).rejects.toThrow();
     });
 
     test('test will throw error on a non-200 response', () => {
       const scope = nock('https://tshnuiufz7.execute-api.us-east-1.amazonaws.com/test')
-        .get('/purchases/consequuntur/consumption')
+        .get('/purchases/ab/consumption')
         .reply(404, { data: {} });
-      return expect(
-        async () => await sdk.purchases.getPurchaseConsumption('consequuntur'),
-      ).rejects.toThrow();
+      return expect(async () => await sdk.purchases.getPurchaseConsumption('ab')).rejects.toThrow();
     });
   });
 });
